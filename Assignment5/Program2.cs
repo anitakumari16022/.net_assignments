71be46c2fb7c372a3a8b0e31c9b5584152d76265
anitakumari16022 <anitakumari90972@gmail.com>
2025-11-03 10:46:42 +0530

    Add MultipleCatchExample class for error handling

---
+++ Assignment5/Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+
+    class MultipleCatchExample
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You entered: " + num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Input must be a valid integer.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: The number is too large or too small for an Int32.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
+        }
+    }
+
+}
