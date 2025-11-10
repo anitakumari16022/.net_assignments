6aa60746f660a97f5a3cf3a171c631caff662ecc
anitakumari16022 <anitakumari90972@gmail.com>
2025-11-10 11:41:05 +0530

    Add files via upload

---
+++ Assignment_6/Program3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLConnectionExample
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=First_Question;Integrated Security=True";
+
+            string query = "SELECT StudentID, Name, Course, Marks FROM Student";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+          
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection established successfully!\n");
+
+                 
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    Console.WriteLine("Student Records:");
+                    Console.WriteLine("------------------------------------------");
+
+                  
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"StudentID: {reader["StudentID"]}, Sname: {reader["Sname"]}, Course: {reader["Course"]}, Marks: {reader["Marks"]}");
+                    }
+
+                   
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("\nConnection closed.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
